4bf01b43c5561c2474fe4a39ed51ba7e37517ea6
BishalAshiq <bishaltheboss20@gmail.com>
2021-10-01 17:30:26 +0600

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className='bg-yellow-200 text-4xl hover:text-blue-100'>This is my text</p>
+      <p className='text-primary'>Hello my text</p>
+      <p className='text-custom'>This mine</p>
     </div>
   );
 }
